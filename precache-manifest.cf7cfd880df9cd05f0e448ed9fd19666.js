44bfb18fd328de6b137cb9715f9b8e1487a1b2db
StancuAndrei <stancuandrei18@yahoo.com>
2020-07-12 16:06:45 +0300

    Updates

---
+++ precache-manifest.cf7cfd880df9cd05f0e448ed9fd19666.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b52398615c6afeef8b02a4be34eeaa5",
+    "url": "/react-breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "dfe57a0facb36f5a99ea",
+    "url": "/react-breaking-bad-cast/static/css/main.585222f1.chunk.css"
+  },
+  {
+    "revision": "f434883eba7e321c471b",
+    "url": "/react-breaking-bad-cast/static/js/2.527e2ae9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-breaking-bad-cast/static/js/2.527e2ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe57a0facb36f5a99ea",
+    "url": "/react-breaking-bad-cast/static/js/main.75165869.chunk.js"
+  },
+  {
+    "revision": "a9a840700b6721d9dadb",
+    "url": "/react-breaking-bad-cast/static/js/runtime-main.721ebe55.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
